bce757836ab4ce2f2b9803d8f2bd79a766e69358
rweilwww <github@shaw-weil.com>
2025-11-03 19:07:50 -0500

    from inDesign 2025-11-04 00:06

--- gitpush.bat
+++ gitpush.bat
@@ -1,14 +1,9 @@
 
 cd /inetpub/wwwroot/w/butler
-git init
-            git config --global --add safe.directory C:/inetpub/wwwroot/w/butler
-   git remote add origin https://github.com/rweilwww/web-butler.git
-   git remote -v
 
-        
 
 git add .
-git commit -m "from inDesign 2025-10-26 01:20" --no-edit
+git commit -m "from inDesign 2025-11-04 00:06" --no-edit
 git push --force --set-upstream origin main
 
 rem wait for one minute to allow git hub to get its act together
