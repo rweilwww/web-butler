4c118d2a90d0ec8997f222d414a88ff20d9c64ac
rweilwww <github@shaw-weil.com>
2026-01-04 19:44:26 -0500

    from inDesign 2026-01-05 00:40

--- gitpush.bat
+++ gitpush.bat
@@ -1,11 +1,9 @@
 
 cd /inetpub/wwwroot/w/butler
 
-
 git add .
-git commit -m "from inDesign 2025-11-04 00:06" --no-edit
+git commit -m "from inDesign 2026-01-05 00:40" --no-edit
 git push --force --set-upstream origin main
-
 rem wait for one minute to allow git hub to get its act together
 timeout 60
 timeout 5
